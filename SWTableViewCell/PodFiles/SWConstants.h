04c1c7e82cc153efcc04b748b5e5a8e8389df1f7
Adam Szeptycki <adamszeptycki@gmail.com>
2014-03-11 09:08:32 +0100

    changes button width

--- SWTableViewCell/PodFiles/SWConstants.h
+++ SWTableViewCell/PodFiles/SWConstants.h
@@ -10,7 +10,7 @@
 #define SWTableViewCell_Constants_h
 
 #define kUtilityButtonsWidthMax 260
-#define kUtilityButtonWidthDefault 90
+#define kUtilityButtonWidthDefault 130  
 #define kSectionIndexWidth 15
 
 #endif
